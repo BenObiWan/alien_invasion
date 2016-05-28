407b0f1e38ae79bcf7ca5286f7e1a08478329067
BenObiWan <benobiwan@gmail.com>
2016-05-28 16:00:28 +0100

    Limit number of bullets to 3

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -22,9 +22,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         # move the ship to the left.
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     """Respond to key releases."""
@@ -32,7 +30,6 @@ def check_keyup_events(event, ship):
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
-
 
 def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
@@ -44,3 +41,19 @@ def update_screen(ai_settings, screen, ship, bullets):
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    # Update bullet positions.
+    bullets.update()
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire a bullet if limit not reached yet."""
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+    
