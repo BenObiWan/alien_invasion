8081c74cb80ea717daa52f01c5ee345f16f43fad
BenObiWan <benobiwan@gmail.com>
2016-05-28 21:57:34 +0100

    Moving the alien fleet

--- game_functions.py
+++ game_functions.py
@@ -76,7 +76,8 @@ def create_alien(ai_settings, screen, aliens, alien_number, row_number):
     """Create an alien and place it in the row."""
     alien = Alien(ai_settings, screen)
     alien_width = alien.rect.width
-    alien.rect.x = alien_width + 2 * alien_width * alien_number
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
     alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
 
@@ -92,3 +93,23 @@ def create_fleet(ai_settings, screen, ship, aliens):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
 
+def check_fleet_edges(ai_settings, aliens):
+     """Respond appropriately if any aliens have reached an edge."""
+     for alien in aliens.sprites():
+         if alien.check_edges():
+             change_fleet_direction(ai_settings, aliens)
+             break
+
+def change_fleet_direction(ai_settings, aliens):
+    """Drop the entire fleet and change the fleet's direction."""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+def update_aliens(ai_settings, aliens):
+    """
+    Check if the fleet is at an edge,
+        and then update the postions of all aliens in the fleet.
+    """
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
